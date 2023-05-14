f4a08d7afea08f21cacedda846b88d9940433cba
mpp <mpp0xdd@gmail.com>
2023-05-14 23:36:25 +0900

    Move tetriminoes by key input

--- MainScreen.java
+++ MainScreen.java
@@ -1,21 +1,44 @@
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class MainScreen extends GameScreen {
+public class MainScreen extends GameScreen implements KeyListener {
 
   private final IField field = new Field();
   private Tetrimino tetrimino = new ITetrimino();
+  private final Point tetriminoLocation = new Point(2, 4);
 
   public MainScreen() {
     super(IField.WIDTH, IField.HEIGHT);
+    setFocusable(true);
+    addKeyListener(this);
   }
 
   @Override
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     field.draw(g, 0, 0);
-    tetrimino.draw(g, 2, 4);
+    tetrimino.draw(g, tetriminoLocation.x, tetriminoLocation.y);
   }
 
   @Override
   protected void runGameLoop() {}
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    switch (e.getKeyCode()) {
+      case KeyEvent.VK_LEFT -> tetriminoLocation.translate(-1, 0);
+      case KeyEvent.VK_RIGHT -> tetriminoLocation.translate(1, 0);
+      case KeyEvent.VK_UP -> tetriminoLocation.translate(0, -1);
+      case KeyEvent.VK_DOWN -> tetriminoLocation.translate(0, 1);
+    }
+    repaint();
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {}
+
+  @Override
+  public void keyTyped(KeyEvent e) {}
 }
