d32522a4f33d2fdc491e89fc5d33a590a653831f
mpp <mpp0xdd@gmail.com>
2023-05-15 00:10:00 +0900

    Change tetrimino area from 4*2 to 4*4

--- ITetrimino.java
+++ ITetrimino.java
@@ -4,9 +4,9 @@ public class ITetrimino extends Tetrimino {
   private static final IBlock block = new ColorBlock(new Color(0, 204, 204));
 
   public ITetrimino() {
-    this.blocks[0][0] = block;
-    this.blocks[1][0] = block;
-    this.blocks[2][0] = block;
-    this.blocks[3][0] = block;
+    this.blocks[0][1] = block;
+    this.blocks[1][1] = block;
+    this.blocks[2][1] = block;
+    this.blocks[3][1] = block;
   }
 }
--- Tetrimino.java
+++ Tetrimino.java
@@ -1,21 +1,22 @@
 import java.awt.Graphics;
 
 public abstract class Tetrimino {
+  private static final int LENGTH = 4;
   protected final IBlock[][] blocks;
 
   public Tetrimino() {
-    this.blocks = new IBlock[4][2];
+    this.blocks = new IBlock[LENGTH][LENGTH];
 
-    for (int i = 0; i < this.blocks.length; i++) {
-      for (int j = 0; j < this.blocks[i].length; j++) {
+    for (int i = 0; i < LENGTH; i++) {
+      for (int j = 0; j < LENGTH; j++) {
         this.blocks[i][j] = IField.EMPTY;
       }
     }
   }
 
   public void draw(Graphics g, int x, int y) {
-    for (int i = 0; i < this.blocks.length; i++) {
-      for (int j = 0; j < this.blocks[i].length; j++) {
+    for (int i = 0; i < LENGTH; i++) {
+      for (int j = 0; j < LENGTH; j++) {
         IBlock block = this.blocks[i][j];
         if (!block.equals(IField.EMPTY)) {
           block.draw(g, (x + j) * IBlock.SIZE, (y + i) * IBlock.SIZE);
