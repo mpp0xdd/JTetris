cb1b65ade585930fb91f995853b076004ae112e7
mpp <mpp0xdd@gmail.com>
2023-05-15 23:15:13 +0900

    Fix variable names

--- MainScreen.java
+++ MainScreen.java
@@ -10,7 +10,7 @@ public class MainScreen extends GameScreen implements KeyListener {
   private ITetrimino tetrimino = supplier.get();
   private final int ix = IField.COLUMNS / 2 - Tetrimino.LENGTH / 2;
   private final int iy = -Tetrimino.LENGTH;
-  private final Point tloc = new Point(ix, iy);
+  private final Point tp = new Point(ix, iy);
 
   public MainScreen() {
     super(IField.WIDTH, IField.HEIGHT);
@@ -23,16 +23,16 @@ public class MainScreen extends GameScreen implements KeyListener {
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     field.draw(g, 0, 0);
-    tetrimino.draw(g, tloc.x, tloc.y);
+    tetrimino.draw(g, tp.x, tp.y);
   }
 
   @Override
   protected void runGameLoop() {
-    if (tetrimino.isSettable(field, tloc.x, tloc.y + 1)) {
-      tloc.translate(0, 1);
+    if (tetrimino.isSettable(field, tp.x, tp.y + 1)) {
+      tp.translate(0, 1);
     } else {
-      tetrimino.set(field, tloc.x, tloc.y);
-      tloc.setLocation(ix, iy);
+      tetrimino.set(field, tp.x, tp.y);
+      tp.setLocation(ix, iy);
       tetrimino = supplier.get();
     }
     repaint();
@@ -42,31 +42,31 @@ public class MainScreen extends GameScreen implements KeyListener {
   public void keyPressed(KeyEvent e) {
     switch (e.getKeyCode()) {
       case KeyEvent.VK_LEFT -> {
-        if (tetrimino.isSettable(field, tloc.x - 1, tloc.y)) {
-          tloc.translate(-1, 0);
+        if (tetrimino.isSettable(field, tp.x - 1, tp.y)) {
+          tp.translate(-1, 0);
         }
       }
       case KeyEvent.VK_RIGHT -> {
-        if (tetrimino.isSettable(field, tloc.x + 1, tloc.y)) {
-          tloc.translate(1, 0);
+        if (tetrimino.isSettable(field, tp.x + 1, tp.y)) {
+          tp.translate(1, 0);
         }
       }
       case KeyEvent.VK_UP -> {
-        if (tetrimino.isSettable(field, tloc.x, tloc.y - 1)) {
-          tloc.translate(0, -1);
+        if (tetrimino.isSettable(field, tp.x, tp.y - 1)) {
+          tp.translate(0, -1);
         }
       }
       case KeyEvent.VK_DOWN -> {
-        if (tetrimino.isSettable(field, tloc.x, tloc.y + 1)) {
-          tloc.translate(0, 1);
+        if (tetrimino.isSettable(field, tp.x, tp.y + 1)) {
+          tp.translate(0, 1);
         } else {
-          tetrimino.set(field, tloc.x, tloc.y);
-          tloc.setLocation(ix, iy);
+          tetrimino.set(field, tp.x, tp.y);
+          tp.setLocation(ix, iy);
           tetrimino = supplier.get();
         }
       }
-      case KeyEvent.VK_A -> tetrimino.rotateLeft(field, tloc.x, tloc.y);
-      case KeyEvent.VK_D -> tetrimino.rotateRight(field, tloc.x, tloc.y);
+      case KeyEvent.VK_A -> tetrimino.rotateLeft(field, tp.x, tp.y);
+      case KeyEvent.VK_D -> tetrimino.rotateRight(field, tp.x, tp.y);
     }
     repaint();
   }
