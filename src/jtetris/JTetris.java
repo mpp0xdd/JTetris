029627d87b54c2defd594db120c4b1e5e159eb03
mpp <mpp0xdd@gmail.com>
2024-06-18 22:00:05 +0900

    WIP

--- src/jtetris/JTetris.java
+++ src/jtetris/JTetris.java
@@ -1,10 +1,17 @@
 package jtetris;
 
+import jglib.base.Game;
 import jglib.component.GameWindow;
 import jtetris.screen.MainScreen;
 
-public class JTetris {
+public class JTetris extends Game {
+
   public static void main(String[] args) {
+    launch(JTetris.class);
+  }
+
+  @Override
+  protected void start() {
     GameWindow window = new GameWindow("JTetris");
     MainScreen screen = new MainScreen();
 
