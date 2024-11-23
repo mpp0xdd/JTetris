910bc08c4c66dc7d73f20b39cb730867ab965b43
mpp <mpp0xdd@gmail.com>
2024-11-23 12:33:43 +0900

    Refactor

--- src/jtetris/component/ColorBlock.java
+++ src/jtetris/component/ColorBlock.java
@@ -6,20 +6,20 @@ import java.util.Objects;
 import jtetris.common.Block;
 import jtetris.resource.ColorFactory;
 
-public class ColorBlock implements Block {
-  public static final Block GRAY = new ColorBlock(ColorFactory.gray());
-  public static final Block BLACK = new ColorBlock(ColorFactory.black());
-  public static final Block LIGHT_BLUE = new ColorBlock(ColorFactory.lightBlue());
-  public static final Block YELLOW = new ColorBlock(ColorFactory.yellow());
-  public static final Block GREEN = new ColorBlock(ColorFactory.green());
-  public static final Block RED = new ColorBlock(ColorFactory.red());
-  public static final Block BLUE = new ColorBlock(ColorFactory.blue());
-  public static final Block ORANGE = new ColorBlock(ColorFactory.orange());
-  public static final Block PURPLE = new ColorBlock(ColorFactory.purple());
+public enum ColorBlock implements Block {
+  GRAY(ColorFactory.gray()),
+  BLACK(ColorFactory.black()),
+  LIGHT_BLUE(ColorFactory.lightBlue()),
+  YELLOW(ColorFactory.yellow()),
+  GREEN(ColorFactory.green()),
+  RED(ColorFactory.red()),
+  BLUE(ColorFactory.blue()),
+  ORANGE(ColorFactory.orange()),
+  PURPLE(ColorFactory.purple());
 
   private final Color color;
 
-  public ColorBlock(Color color) {
+  private ColorBlock(Color color) {
     this.color = Objects.requireNonNull(color);
   }
 
@@ -27,19 +27,5 @@ public class ColorBlock implements Block {
   public void draw(Graphics g, int x, int y) {
     g.setColor(this.color);
     g.fill3DRect(x, y, size(), size(), false);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(color);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null) return false;
-    if (getClass() != obj.getClass()) return false;
-    ColorBlock other = (ColorBlock) obj;
-    return Objects.equals(color, other.color);
   }
 }
--- src/jtetris/component/ImageBlock.java
+++ src/jtetris/component/ImageBlock.java
@@ -2,26 +2,24 @@ package jtetris.component;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.net.URL;
 import java.util.Objects;
-import jglib.util.GameUtilities;
 import jtetris.common.Block;
 import jtetris.resource.ImageFactory;
 
-public class ImageBlock implements Block {
-  public static final Block GRAY = new ImageBlock(ImageFactory.newGrayBlockImage());
-  public static final Block BLACK = new ImageBlock(ImageFactory.newBlackBlockImage());
-  public static final Block LIGHT_BLUE = new ImageBlock(ImageFactory.newLightBlueBlockImage());
-  public static final Block YELLOW = new ImageBlock(ImageFactory.newYellowBlockImage());
-  public static final Block GREEN = new ImageBlock(ImageFactory.newGreenBlockImage());
-  public static final Block RED = new ImageBlock(ImageFactory.newRedBlockImage());
-  public static final Block BLUE = new ImageBlock(ImageFactory.newBlueBlockImage());
-  public static final Block ORANGE = new ImageBlock(ImageFactory.newOrangeBlockImage());
-  public static final Block PURPLE = new ImageBlock(ImageFactory.newPurpleBlockImage());
+public enum ImageBlock implements Block {
+  GRAY(ImageFactory.newGrayBlockImage()),
+  BLACK(ImageFactory.newBlackBlockImage()),
+  LIGHT_BLUE(ImageFactory.newLightBlueBlockImage()),
+  YELLOW(ImageFactory.newYellowBlockImage()),
+  GREEN(ImageFactory.newGreenBlockImage()),
+  RED(ImageFactory.newRedBlockImage()),
+  BLUE(ImageFactory.newBlueBlockImage()),
+  ORANGE(ImageFactory.newOrangeBlockImage()),
+  PURPLE(ImageFactory.newPurpleBlockImage());
 
   private final BufferedImage image;
 
-  public ImageBlock(BufferedImage image) {
+  private ImageBlock(BufferedImage image) {
     this.image = Objects.requireNonNull(image);
     if (image.getWidth() != size() || image.getHeight() != size()) {
       throw (new IllegalArgumentException(
@@ -31,26 +29,8 @@ public class ImageBlock implements Block {
     }
   }
 
-  public ImageBlock(URL url) {
-    this(GameUtilities.loadImage(url).orElseThrow());
-  }
-
   @Override
   public void draw(Graphics g, int x, int y) {
     g.drawImage(image, x, y, null);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(image);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null) return false;
-    if (getClass() != obj.getClass()) return false;
-    ImageBlock other = (ImageBlock) obj;
-    return Objects.equals(image, other.image);
   }
 }
